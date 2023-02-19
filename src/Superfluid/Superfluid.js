31c8166544c73b0b52498502dfa5123fe2bbd16a
Laxmi-Sreenivas <96071388+Laxmi-Sreenivas@users.noreply.github.com>
2023-02-19 23:52:17 +0530

    Update Superfluid.js

--- src/Superfluid/Superfluid.js
+++ src/Superfluid/Superfluid.js
@@ -1,30 +1,14 @@
 import { ethers } from "ethers";
 
 //Gets Called As Soon as User is Chosen for the Job
-async function createNewFlow(recipient, flowRate) {
-
+async function createNewFlow(receiver, speed) {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     await provider.send("eth_requestAccounts", []);
-
     const signer = provider.getSigner();
-
     const chainId = await window.ethereum.request({ method: "eth_chainId" });
-    const sf = await Framework.create({
-        chainId: Number(chainId),
-        provider: provider
-    });
-
-    const superSigner = sf.createSigner({ signer: signer });
-
-    console.log(await superSigner.getAddress());
-    const daix = await sf.loadSuperToken("fDAIx");
-
-
-    const createFlowOperation = daix.createFlow({
-        sender: await superSigner.getAddress(),
-        receiver: recipient,
-        flowRate: flowRate
-    });
-
+    const stream = await Framework.create({chainId: Number(chainId),provider: provider});
+    const superSigner = stream.createSigner({ signer: signer });
+    const daix = await stream.loadSuperToken("fDAIx");
+    const createFlowOperation = daix.createFlow({sender: await superSigner.getAddress(),receiver: receiver,speed: speed});
     const result = await createFlowOperation.exec(superSigner);
 }
